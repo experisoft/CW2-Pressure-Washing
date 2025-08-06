aaf8bd2f91f2370065d17e247f105454f5118e2b
Alan Prophett <AAP9002@gmail.com>
2025-08-06 22:47:24 +0100

    Update astro.config.mjs for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://experisoft.github.io',
-    base: 'CW2-Pressure-Washing'
+    site: 'https://cw2pressurewashing.com/'
 });
